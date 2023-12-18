813e1bc845dc2ee74aaf1fc74eb857d1539e0bd9
Eckry <112786854+Eckry@users.noreply.github.com>
2023-12-18 17:04:28 -0600

    improving preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,10 @@
-import "./styles/Preview.css"
+import "./styles/Preview.css";
 
-export default function Preview({product}){
+export default function Preview({ product }) {
   return (
     <div className="preview-container">
+      <img className="preview-image" src={product.image} alt="product image" />
       <div className="description-container">
-        <img className="preview-image" src={product.image} alt="product image" />
         <div className="title-container">
           <h1 className="title">{product.title}</h1>
           <p className="description">{product.description}</p>
@@ -13,8 +13,10 @@ export default function Preview({product}){
       </div>
       <div className="rating-container">
         <h1>{product.price}</h1>
-        <p>{product.rating.rate} {product.rating.count}</p>
+        <p>
+          {product.rating.rate} {product.rating.count}
+        </p>
       </div>
     </div>
-  )
+  );
 }
