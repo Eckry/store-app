62f6c83b4cc264969f2c8eadba57de670ad86ec5
Eckry <112786854+Eckry@users.noreply.github.com>
2023-12-30 19:31:38 -0600

    add responsive container

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,37 +1,34 @@
 import RangeInput from "./RangeInput";
 import CheckBox from "./CheckBox";
 import categories from "../categories.json";
-import "./styles/Filters.css"
+import "./styles/Filters.css";
 
 export default function Filters({
   setPriceFilter,
   price,
   setSearchedCategories,
   showFilters,
-  currentPreview
+  currentPreview,
 }) {
+  const displayFilters = Object.keys(currentPreview).length !== 0;
 
-  const displayFilters = Object.keys(currentPreview).length !== 0
-
-  if(displayFilters) return;
+  if (displayFilters) return;
 
   return (
     <div className={showFilters ? "filters" : "filters-hide"}>
-      <div className="padding">
-        <h3 className="filters-title">Price range</h3>
-        <div className="price-range-container">
-          <RangeInput onChange={setPriceFilter} price={price} />
-        </div>
-        <h3 className="filters-title">Categories</h3>
-        <div className="checkboxes-container">
-          {categories.map((category) => (
-            <CheckBox
-              category={category}
-              setSearchedCategories={setSearchedCategories}
-              key={category}
-            />
-          ))}
-        </div>
+      <h3 className="filters-title">Price range</h3>
+      <div className="price-range-container">
+        <RangeInput onChange={setPriceFilter} price={price} />
+      </div>
+      <h3 className="filters-title">Categories</h3>
+      <div className="checkboxes-container">
+        {categories.map((category) => (
+          <CheckBox
+            category={category}
+            setSearchedCategories={setSearchedCategories}
+            key={category}
+          />
+        ))}
       </div>
     </div>
   );
