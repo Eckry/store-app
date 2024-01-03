a59448af7c6f138af480f8dbeaaf7f835dddc568
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-02 22:30:21 -0600

    add style to carousel buttons

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,13 +1,11 @@
-export default function PageButton({ currentPage, onClick, children, value}) {
+import "./styles/PageButton.css";
+
+export default function PageButton({ currentPage, onClick, children, value }) {
   return (
     <button
       value={value}
       onClick={(e) => onClick(e)}
-      style={
-        currentPage === value
-          ? { backgroundColor: "yellowgreen" }
-          : { backgroundColor: "white" }
-      }
+      className={currentPage === value ? "page-button-selected" : "page-button"}
     >
       {children}
     </button>
