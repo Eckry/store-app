41a35b1d2a3cf31f4726546fe805665be03c8caa
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-04 20:00:49 -0600

    add styles to preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -29,8 +29,39 @@ export default function Preview({
   }
 
   return (
-    <div className="preview-container">
-      <div className="image-container">
+    <>
+      <div className="preview-container">
+        <div className="image-and-description-container">
+          <img
+            className="preview-image"
+            src={product.image}
+            alt="product image"
+          />
+          <div className="description-container">
+            <h1 className="title" title={product.title}>
+              {product.title}
+            </h1>
+            <h4 className="subtitle">Description</h4>
+            <p className="description">{product.description}</p>
+            <p className="category">{product.category}</p>
+          </div>
+        </div>
+        <div className="rating-container">
+          <h1 className="product-price">{product.price}$</h1>
+          <Stars stars={product.rating.rate} count={product.rating.count} />
+        </div>
+
+        <button
+          className="add-to-cart-button"
+          onClick={handleAddProductToShoppingCart}
+        >
+          Add to cart
+        </button>
+        <button className="return-button" onClick={handleSetCurrentPreview}>
+          <IoReturnUpBackOutline className="io-icon" />
+        </button>
+      </div>
+      <div className="nav-buttons-container">
         <button
           className={`nav-button${!showPrev ? " disabled" : ""}`}
           disabled={!showPrev}
@@ -38,11 +69,6 @@ export default function Preview({
         >
           <FaLongArrowAltLeft />
         </button>
-        <img
-          className="preview-image"
-          src={product.image}
-          alt="product image"
-        />
         <button
           className={`nav-button${!showNext ? " disabled" : ""}`}
           disabled={!showNext}
@@ -51,27 +77,6 @@ export default function Preview({
           <FaLongArrowAltRight />
         </button>
       </div>
-      <div className="description-container">
-        <h1 className="title" title={product.title}>
-          {product.title}
-        </h1>
-        <h4 className="subtitle">Description</h4>
-        <p className="description">{product.description}</p>
-        <p className="category">{product.category}</p>
-        <div className="rating-container">
-          <h1 className="product-price">{product.price}$</h1>
-          <Stars stars={product.rating.rate} count={product.rating.count} />
-        </div>
-      </div>
-      <button
-        className="add-to-cart-button"
-        onClick={handleAddProductToShoppingCart}
-      >
-        ADD TO CART
-      </button>
-      <button className="return-button" onClick={handleSetCurrentPreview}>
-        <IoReturnUpBackOutline className="io-icon" />
-      </button>
-    </div>
+    </>
   );
 }
