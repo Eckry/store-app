59cabe07ee9641d7ba8d0a626333a535e6bfd714
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-15 17:54:48 -0600

    add label to each input

--- src/routes/Address.jsx
+++ src/routes/Address.jsx
@@ -32,57 +32,81 @@ export default function Address() {
     <div>
       <form className="address-form" onSubmit={handleSubmit}>
         <div className="address-wrapper">
-          <input
-            className="address-input"
-            type="text"
-            name="firstName"
-            defaultValue={data.firstName}
-          />
-          <input
-            className="address-input"
-            type="text"
-            name="lastName"
-            defaultValue={data.lastName}
-          />
+          <label htmlFor="" className="form-label">
+            First Name
+            <input
+              className="address-input"
+              type="text"
+              name="firstName"
+              defaultValue={data.firstName}
+            />
+          </label>
+          <label htmlFor="" className="form-label">
+            Last Name
+            <input
+              className="address-input"
+              type="text"
+              name="lastName"
+              defaultValue={data.lastName}
+            />
+          </label>
         </div>
-        <input
-          className="address-input"
-          type="text"
-          name="address"
-          defaultValue={data.address}
-        />
-        <input
-          className="address-input"
-          type="text"
-          name="zipCode"
-          defaultValue={data.zipCode}
-        />
+        <label htmlFor="" className="form-label">
+          Address
+          <input
+            className="address-input"
+            type="text"
+            name="address"
+            defaultValue={data.address}
+          />
+        </label>
+        <label htmlFor="" className="form-label">
+          Zip Code
+          <input
+            className="address-input"
+            type="text"
+            name="zipCode"
+            defaultValue={data.zipCode}
+          />
+        </label>
         <div className="address-wrapper">
-          <input
-            className="address-input"
-            type="text"
-            name="city"
-            defaultValue={data.city}
-          />
-          <input
-            className="address-input"
-            type="text"
-            name="country"
-            defaultValue={data.country}
-          />
+          <label htmlFor="" className="form-label">
+            City
+            <input
+              className="address-input"
+              type="text"
+              name="city"
+              defaultValue={data.city}
+            />
+          </label>
+          <label htmlFor="" className="form-label">
+            Country
+            <input
+              className="address-input"
+              type="text"
+              name="country"
+              defaultValue={data.country}
+            />
+          </label>
         </div>
-        <input
-          className="address-input"
-          type="tel"
-          name="phone"
-          defaultValue={data.phone}
-        />
-        <input
-          className="address-input"
-          type="email"
-          name="email"
-          defaultValue={data.emailx}
-        />
+        <label htmlFor="" className="form-label">
+          Phone
+          <input
+            className="address-input"
+            type="tel"
+            name="phone"
+            defaultValue={data.phone}
+          />
+        </label>
+        <label htmlFor="" className="form-label">
+          Email
+          <input
+            className="address-input"
+            type="email"
+            name="email"
+            defaultValue={data.emailx}
+          />
+        </label>
       </form>
       <button className="address-button" type="submit">
         Next {"->"}
