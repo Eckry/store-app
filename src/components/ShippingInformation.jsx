0f9d0b9b1152b5e4c816c474a7445bf620c0ee53
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-15 19:25:54 -0600

    make a shopping component

---
+++ src/components/ShippingInformation.jsx
@@ -0,0 +1,29 @@
+import "./styles/ShippingInformation.css";
+import { Link } from "react-router-dom";
+
+export default function ShippingInformation({data}) {
+  return (
+    <div>
+      <main className="shipping-container">
+        <Link to={"/store-app/checkout/data"} className="shipping-button edit">
+          Edit
+        </Link>
+        <h2 className="shipping-title">Your information</h2>
+        <ul>
+          <li className="user-data">
+            {data.firstName} {data.lastName}
+          </li>
+          <li className="user-data">{data.address}</li>
+          <li className="user-data">{data.zipCode}</li>
+          <li className="user-data">{data.city}</li>
+          <li className="user-data">{data.country}</li>
+          <li className="user-data">{data.phone}</li>
+          <li className="user-data">{data.email}</li>
+        </ul>
+      </main>
+      <Link to={"/store-app/checkout/payment"} className="shipping-button next">
+        Next {"->"}
+      </Link>
+    </div>
+  );
+}
--- src/routes/Shipping.jsx
+++ src/routes/Shipping.jsx
@@ -1,28 +1,12 @@
-import "./styles/Shipping.css";
 import useAddress from "../hooks/useAddress";
 import { Link } from "react-router-dom";
+import ShippingInformation from "../components/ShippingInformation";
 
 export default function Shipping() {
   const { data } = useAddress();
   return (
     <div >
-      <main className="shipping-container">
-        <Link to={"/store-app/checkout/data"} className="shipping-button edit">
-          Edit
-        </Link>
-        <h2 className="shipping-title">Your information</h2>
-        <ul>
-          <li className="user-data">
-            {data.firstName} {data.lastName}
-          </li>
-          <li className="user-data">{data.address}</li>
-          <li className="user-data">{data.zipCode}</li>
-          <li className="user-data">{data.city}</li>
-          <li className="user-data">{data.country}</li>
-          <li className="user-data">{data.phone}</li>
-          <li className="user-data">{data.email}</li>
-        </ul>
-      </main>
+      <ShippingInformation data={data} />
       <Link to={"/store-app/checkout/payment"} className="shipping-button next">
         Next {"->"}
       </Link>
