a257d7726ba234237035346586220e3aca330290
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-17 19:40:26 -0600

    add styles to address form

--- src/routes/Address.jsx
+++ src/routes/Address.jsx
@@ -28,105 +28,114 @@ export default function Address() {
       phone: phone.value,
       email: email.value,
     };
-    console.log("sss");
     updateData(newData);
     return navigate("/store-app/checkout/shipping");
   }
 
   return (
     <form className="address-form" onSubmit={handleSubmit}>
-      <div className="address-wrapper">
-        <label htmlFor="firstName" className="form-label">
-          First Name
+      <div className="address-container">
+        <div className="address-wrapper">
+          <label htmlFor="firstName" className="form-label">
+            First Name
+            <input
+              id="firstName"
+              required
+              className="address-input"
+              type="text"
+              name="firstName"
+              defaultValue={data.firstName}
+              placeholder="e.g. John"
+            />
+          </label>
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+            <input
+              id="lastName"
+              required
+              className="address-input"
+              type="text"
+              name="lastName"
+              defaultValue={data.lastName}
+              placeholder="e.g. Doe"
+            />
+          </label>
+        </div>
+        <label htmlFor="address" className="form-label">
+          Address
           <input
-            id="firstName"
+            id="address"
             required
             className="address-input"
             type="text"
-            name="firstName"
-            defaultValue={data.firstName}
+            name="address"
+            defaultValue={data.address}
+            placeholder="e.g. 1313 E Main St, Portage"
           />
         </label>
-        <label htmlFor="lastName" className="form-label">
-          Last Name
+        <label htmlFor="zipCode" className="form-label">
+          Zip Code
           <input
-            id="lastName"
+            id="zipCode"
             required
             className="address-input"
             type="text"
-            name="lastName"
-            defaultValue={data.lastName}
+            name="zipCode"
+            defaultValue={data.zipCode}
+            placeholder="e.g. 90210"
+          />
+        </label>
+        <div className="address-wrapper">
+          <label htmlFor="city" className="form-label">
+            City
+            <input
+              id="city"
+              required
+              className="address-input"
+              type="text"
+              name="city"
+              defaultValue={data.city}
+              placeholder="e.g. Berlin"
+            />
+          </label>
+          <label htmlFor="country" className="form-label">
+            Country
+            <input
+              id="country"
+              required
+              className="address-input"
+              type="text"
+              name="country"
+              defaultValue={data.country}
+              placeholder="e.g. Mexico"
+            />
+          </label>
+        </div>
+        <label htmlFor="phone" className="form-label">
+          Phone
+          <input
+            id="phone"
+            required
+            className="address-input"
+            type="tel"
+            name="phone"
+            defaultValue={data.phone}
+            placeholder="e.g. +52 659 123 4545"
+          />
+        </label>
+        <label htmlFor="email" className="form-label">
+          Email
+          <input
+            id="email"
+            required
+            className="address-input"
+            type="email"
+            name="email"
+            defaultValue={data.email}
+            placeholder="e.g. example@gmail.com"
           />
         </label>
       </div>
-      <label htmlFor="address" className="form-label">
-        Address
-        <input
-          id="address"
-          required
-          className="address-input"
-          type="text"
-          name="address"
-          defaultValue={data.address}
-        />
-      </label>
-      <label htmlFor="zipCode" className="form-label">
-        Zip Code
-        <input
-          id="zipCode"
-          required
-          className="address-input"
-          type="text"
-          name="zipCode"
-          defaultValue={data.zipCode}
-        />
-      </label>
-      <div className="address-wrapper">
-        <label htmlFor="city" className="form-label">
-          City
-          <input
-            id="city"
-            required
-            className="address-input"
-            type="text"
-            name="city"
-            defaultValue={data.city}
-          />
-        </label>
-        <label htmlFor="country" className="form-label">
-          Country
-          <input
-            id="country"
-            required
-            className="address-input"
-            type="text"
-            name="country"
-            defaultValue={data.country}
-          />
-        </label>
-      </div>
-      <label htmlFor="phone" className="form-label">
-        Phone
-        <input
-          id="phone"
-          required
-          className="address-input"
-          type="tel"
-          name="phone"
-          defaultValue={data.phone}
-        />
-      </label>
-      <label htmlFor="email" className="form-label">
-        Email
-        <input
-          id="email"
-          required
-          className="address-input"
-          type="email"
-          name="email"
-          defaultValue={data.email}
-        />
-      </label>
       <button className="address-button" type="submit">
         Next {"->"}
       </button>
