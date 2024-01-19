e035d35bc2a62173962e234cf8da9b410cf9510c
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-18 22:56:55 -0600

    fix shipping information

--- src/components/ShippingInformation.jsx
+++ src/components/ShippingInformation.jsx
@@ -1,43 +1,65 @@
 import "./styles/ShippingInformation.css";
 import { Link } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
 
 export default function ShippingInformation({ data }) {
   return (
-    <div>
-      <main className="shipping-container">
-        <Link to={"/store-app/checkout/data"} className="shipping-button edit">
-          Edit
-        </Link>
+    <main className="shipping-container">
+      <header className="shipping-header">
         <h2 className="shipping-title">
           Check if your information is correct please.
         </h2>
-        <ul className="user-data-container">
-          <li className="user-data">
-            Name
-            <span title={`${data.firstName} ${data.lastName}`} className="user-data-title">
-              {data.firstName} {data.lastName}
-            </span>
-          </li>
-          <li className="user-data">
-            Address <span title={data.address} className="user-data-title">{data.address}</span>
-          </li>
-          <li className="user-data">
-            Zip Code <span title={data.zipcode} className="user-data-title">{data.zipCode}</span>
-          </li>
-          <li className="user-data">
-            City <span title={data.city} className="user-data-title">{data.city}</span>
-          </li>
-          <li className="user-data">
-            Country <span title={data.country} className="user-data-title">{data.country}</span>
-          </li>
-          <li className="user-data">
-            Phone <span title={data.phone} className="user-data-title">{data.phone}</span>
-          </li>
-          <li className="user-data">
-            Email <span title={data.email} className="user-data-title">{data.email}</span>
-          </li>
-        </ul>
-      </main>
-    </div>
+        <Link to={"/store-app/checkout/data"} className="edit">
+          Edit <FaEdit />
+        </Link>
+      </header>
+      <ul className="user-data-container">
+        <li className="user-data">
+          Name
+          <span
+            title={`${data.firstName} ${data.lastName}`}
+            className="user-data-title"
+          >
+            {data.firstName} {data.lastName}
+          </span>
+        </li>
+        <li className="user-data">
+          Address
+          <span title={data.address} className="user-data-title">
+            {data.address}
+          </span>
+        </li>
+        <li className="user-data">
+          Zip Code
+          <span title={data.zipcode} className="user-data-title">
+            {data.zipCode}
+          </span>
+        </li>
+        <li className="user-data">
+          City
+          <span title={data.city} className="user-data-title">
+            {data.city}
+          </span>
+        </li>
+        <li className="user-data">
+          Country
+          <span title={data.country} className="user-data-title">
+            {data.country}
+          </span>
+        </li>
+        <li className="user-data">
+          Phone
+          <span title={data.phone} className="user-data-title">
+            {data.phone}
+          </span>
+        </li>
+        <li className="user-data">
+          Email
+          <span title={data.email} className="user-data-title">
+            {data.email}
+          </span>
+        </li>
+      </ul>
+    </main>
   );
 }
--- src/routes/Shipping.jsx
+++ src/routes/Shipping.jsx
@@ -1,14 +1,15 @@
 import useAddress from "../hooks/useAddress";
 import { Link } from "react-router-dom";
 import ShippingInformation from "../components/ShippingInformation";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 export default function Shipping() {
   const { data } = useAddress();
   return (
-    <div >
+    <div className="confirmation-container">
       <ShippingInformation data={data} />
-      <Link to={"/store-app/checkout/payment"} className="shipping-button next">
-        Next {"->"}
+      <Link to={"/store-app/checkout/payment"} className="shipping-button">
+        Next <FaLongArrowAltRight />
       </Link>
     </div>
   );
