44b4125aaf4c606de25d6039cbe4adfb68e27031
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-18 22:23:54 -0600

    add styles to confirmation page

--- src/components/ShippingInformation.jsx
+++ src/components/ShippingInformation.jsx
@@ -1,24 +1,41 @@
 import "./styles/ShippingInformation.css";
 import { Link } from "react-router-dom";
 
-export default function ShippingInformation({data}) {
+export default function ShippingInformation({ data }) {
   return (
     <div>
       <main className="shipping-container">
         <Link to={"/store-app/checkout/data"} className="shipping-button edit">
           Edit
         </Link>
-        <h2 className="shipping-title">Your information</h2>
-        <ul>
+        <h2 className="shipping-title">
+          Check if your information is correct please.
+        </h2>
+        <ul className="user-data-container">
           <li className="user-data">
-            {data.firstName} {data.lastName}
+            Name
+            <span title={`${data.firstName} ${data.lastName}`} className="user-data-title">
+              {data.firstName} {data.lastName}
+            </span>
           </li>
-          <li className="user-data">{data.address}</li>
-          <li className="user-data">{data.zipCode}</li>
-          <li className="user-data">{data.city}</li>
-          <li className="user-data">{data.country}</li>
-          <li className="user-data">{data.phone}</li>
-          <li className="user-data">{data.email}</li>
+          <li className="user-data">
+            Address <span title={data.address} className="user-data-title">{data.address}</span>
+          </li>
+          <li className="user-data">
+            Zip Code <span title={data.zipcode} className="user-data-title">{data.zipCode}</span>
+          </li>
+          <li className="user-data">
+            City <span title={data.city} className="user-data-title">{data.city}</span>
+          </li>
+          <li className="user-data">
+            Country <span title={data.country} className="user-data-title">{data.country}</span>
+          </li>
+          <li className="user-data">
+            Phone <span title={data.phone} className="user-data-title">{data.phone}</span>
+          </li>
+          <li className="user-data">
+            Email <span title={data.email} className="user-data-title">{data.email}</span>
+          </li>
         </ul>
       </main>
     </div>
