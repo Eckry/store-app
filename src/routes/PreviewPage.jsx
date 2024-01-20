f1e94b640e88c99cc6be40054767274460e474eb
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-19 21:46:49 -0600

    move preview component to a page

--- src/routes/PreviewPage.jsx
+++ src/routes/PreviewPage.jsx
@@ -1,10 +1,16 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import "./styles/Preview.css";
+import { useParams } from "react-router-dom";
 import Footer from "../components/Footer";
-import Preview from "../components/Preview";
 import useFilters from "../hooks/useFilters";
+import useCart from "../hooks/useCart";
+import Stars from "../components/Stars";
+import { Link } from "react-router-dom";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import { IoReturnUpBackOutline } from "react-icons/io5";
 
 export default function PreviewPage() {
   const { filteredProducts } = useFilters();
+  const { addProduct } = useCart();
   const { id } = useParams();
 
   const previewIndex = filteredProducts.findIndex(
@@ -13,17 +19,60 @@ export default function PreviewPage() {
 
   let prev = filteredProducts[previewIndex - 1];
   let next = filteredProducts[previewIndex + 1];
+  const product = filteredProducts[previewIndex];
 
-  const preview = {
-    product: filteredProducts[previewIndex],
-    prev,
-    next,
-  };
+  function handleAddProductToShoppingCart() {
+    addProduct(product);
+  }
   return (
     <>
-      <Preview
-        preview={preview}
-      />
+      <>
+        <div className="preview-container">
+          <div className="image-and-description-container">
+            <img
+              className="preview-image"
+              src={`../${product.image}`}
+              alt="product image"
+            />
+            <div className="description-container">
+              <h1 className="title" title={product.title}>
+                {product.title}
+              </h1>
+              <h4 className="subtitle">Description</h4>
+              <p className="description">{product.description}</p>
+              <p className="category">{product.category}</p>
+            </div>
+          </div>
+          <div className="rating-container">
+            <h1 className="product-price">{product.price}$</h1>
+            <Stars stars={product.rating.rate} count={product.rating.count} />
+          </div>
+
+          <button
+            className="add-to-cart-button"
+            onClick={handleAddProductToShoppingCart}
+          >
+            Add to cart
+          </button>
+          <Link to={"/store-app/"} className="return-button">
+            <IoReturnUpBackOutline className="io-icon" />
+          </Link>
+        </div>
+        <div className="nav-buttons-container">
+          <Link
+            to={`/store-app/preview/${prev?.id}`}
+            className={prev ? "nav-button" : "disabled"}
+          >
+            <FaLongArrowAltLeft />
+          </Link>
+          <Link
+            to={`/store-app/preview/${next?.id}`}
+            className={next ? "nav-button" : "disabled"}
+          >
+            <FaLongArrowAltRight />
+          </Link>
+        </div>
+      </>
       <Footer />
     </>
   );
