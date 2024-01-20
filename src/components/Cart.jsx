2cb2092e1dfc2af84f03233d71e8a98beaa7e665
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-19 20:32:54 -0600

    fix empty cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Stars from "./Stars";
 import { BiPurchaseTag } from "react-icons/bi";
+import { IoReturnUpBackOutline } from "react-icons/io5";
 import useCart from "../hooks/useCart";
 import { Link } from "react-router-dom";
 import { LiaCartArrowDownSolid } from "react-icons/lia";
@@ -36,91 +37,84 @@ export default function Cart() {
 
   if (!cart.length)
     return (
-      <>
-        <Link to="/store-app/" className="close-button">
-          <MdClose />
+      <div className="cart-container-empty">
+        <MdOutlineRemoveShoppingCart />
+        <h1 className="empty-title">Your cart is empty!</h1>
+        <Link to="/store-app/" className="continue-shopping-empty">
+          <IoReturnUpBackOutline /> Continue shopping
         </Link>
-        <div className="cart-container-empty">
-          <MdOutlineRemoveShoppingCart />
-          <h1 className="empty-title">Your cart is empty!</h1>
-        </div>
-      </>
+      </div>
     );
 
   return (
-    <>
-      <Link to="/store-app/" className="close-button">
-        <MdClose />
-      </Link>
-      <div className="cart-container">
-        <div className="product-selected-container">
-            <img
-              draggable="false"
-              className="product-selected-image"
-              src={productSelected.image}
-              alt={productSelected.title}
+    <div className="cart-container">
+      <div className="product-selected-container">
+        <img
+          draggable="false"
+          className="product-selected-image"
+          src={productSelected.image}
+          alt={productSelected.title}
+        />
+        <div className="product-selected-information">
+          <h1 className="product-selected-title">{productSelected.title}</h1>
+          <div>
+            <h4 className="cart-description-title">Description</h4>
+            <p className="product-selected-description">
+              {productSelected.description}
+            </p>
+            <p className="category">{productSelected.category}</p>
+          </div>
+          <div className="cart-rating">
+            <p className="cart-price">{productSelected.price}$</p>
+            <Stars
+              stars={productSelected.rating.rate}
+              count={productSelected.rating.count}
             />
-          <div className="product-selected-information">
-            <h1 className="product-selected-title">{productSelected.title}</h1>
-            <div>
-              <h4 className="cart-description-title">Description</h4>
-              <p className="product-selected-description">
-                {productSelected.description}
-              </p>
-              <p className="category">{productSelected.category}</p>
-            </div>
-            <div className="cart-rating">
-              <p className="cart-price">{productSelected.price}$</p>
-              <Stars
-                stars={productSelected.rating.rate}
-                count={productSelected.rating.count}
-              />
-            </div>
           </div>
-        </div>
-        <div className="buttons">
-          <div className="cart-buttons">
-            <div className="cart-buttons-container">
-              <button onClick={handleRemove} className="sell-button">
-                <FaChevronLeft />
-              </button>
-              <p className="total">{productSelected.quantity}</p>
-              <button onClick={handleBuyProduct} className="buy-button">
-                <FaChevronRight />
-              </button>
-            </div>
-            <div className="buy-and-trash-buttons">
-              <Link
-                to="/store-app/checkout/data"
-                className="buy-product-anchor"
-              >
-                <BiPurchaseTag />
-              </Link>
-              <button className="trash-button" onClick={handleDelete}>
-                <FaRegTrashAlt />
-              </button>
-            </div>
-          </div>
-          <Link to="/store-app/checkout/data" className="buy-everything">
-            <LiaCartArrowDownSolid />
-          </Link>
-        </div>
-        <div className="other-products">
-          {cart.map((product) => {
-            return (
-              <img
-                draggable="false"
-                id={product.id}
-                onClick={() => handleChangeProductSelectedInCart(product)}
-                src={product.image}
-                className="other-products-image"
-                key={product.id}
-                alt={product.title}
-              />
-            );
-          })}
         </div>
       </div>
-    </>
+      <div className="buttons">
+        <div className="cart-buttons">
+          <div className="cart-buttons-container">
+            <button onClick={handleRemove} className="sell-button">
+              <FaChevronLeft />
+            </button>
+            <p className="total">{productSelected.quantity}</p>
+            <button onClick={handleBuyProduct} className="buy-button">
+              <FaChevronRight />
+            </button>
+          </div>
+          <div className="buy-and-trash-buttons">
+            <Link to="/store-app/checkout/data" className="buy-product-anchor">
+              <BiPurchaseTag />
+            </Link>
+            <button className="trash-button" onClick={handleDelete}>
+              <FaRegTrashAlt />
+            </button>
+          </div>
+        </div>
+        <Link to="/store-app/checkout/data" className="buy-everything">
+          <LiaCartArrowDownSolid />
+        </Link>
+      </div>
+      <div className="other-products">
+        {cart.map((product) => {
+          return (
+            <img
+              draggable="false"
+              id={product.id}
+              onClick={() => handleChangeProductSelectedInCart(product)}
+              src={product.image}
+              className="other-products-image"
+              key={product.id}
+              alt={product.title}
+            />
+          );
+        })}
+      </div>
+      <Link to="/store-app/" className="continue-shopping margin-top">
+        <IoReturnUpBackOutline /> Continue shopping
+      </Link>
+    </div>
   );
 }
