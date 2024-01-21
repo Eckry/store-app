fb1a2227cb950c2b0cd3136e9ddbd5364f4df013
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-20 20:07:59 -0600

    add warning link icon

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -1,5 +1,6 @@
-import { IoIosWarning } from "react-icons/io";
 import "./styles/Warning.css";
+import { IoIosWarning } from "react-icons/io";
+import { FaLink } from "react-icons/fa";
 
 export default function Warning() {
   return (
@@ -7,7 +8,7 @@ export default function Warning() {
       <IoIosWarning className="warning-icon" />
       <p className="warning-text">
         I do not sell these products, if you want to buy the actual products you
-        saw here, click on the ICON besides each product, whatever, if you
+        saw here, click on the <FaLink className="warning-link"/> besides each product, whatever, if you
         continue with the payment process, you will give me a donation of $1.
       </p>
     </div>
