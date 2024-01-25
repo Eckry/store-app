f0858e14f26005279e1d215db225165e399e2323
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-24 20:37:40 -0600

    fix home path

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,17 +1,8 @@
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
-import { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
 import { ScrollRestoration } from "react-router-dom";
 
 export default function Root() {
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (pathname === "/store-app/") navigate("/store-app/home");
-  }, []);
-
   return (
     <>
       <ScrollRestoration getKey={(location) => location.pathname} />
