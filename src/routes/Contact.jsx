7f94f7bbb50778b9a28f8db4631a38c5ce790ed6
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-27 17:15:38 -0600

    add contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,8 +1,54 @@
+import "./styles/Contact.css";
 
 export default function Contact() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <main className="contact-container">
+      <form onSubmit={handleSubmit} className="address-form">
+        <div className="address-container">
+          <header className="contact-header">
+            <h1>Send me an email</h1>
+            <p>My email is ericktorresagui@gmail.com</p>
+          </header>
+          <div className="address-wrapper">
+            <label htmlFor="contact-firstName" className="form-label">
+              First name
+              <input
+                className="address-input"
+                type="text"
+                id="contact-firstName"
+              />
+            </label>
+            <label htmlFor="contact-lastName" className="form-label">
+              Last name
+              <input
+                className="address-input"
+                type="text"
+                id="contact-lastName"
+              />
+            </label>
+          </div>
+          <label htmlFor="contact-email" className="form-label">
+            Email
+            <input className="address-input" type="emsail" id="contact-email" />
+          </label>
+          <label htmlFor="contact-area" className="form-label">
+            Message
+            <textarea
+              className="address-input contact-area"
+              name=""
+              id="contact-area"
+              rows="10"
+            ></textarea>
+          </label>
+        </div>
+        <button type="submit" className="shipping-button">
+          Send
+        </button>
+      </form>
+    </main>
   );
 }
